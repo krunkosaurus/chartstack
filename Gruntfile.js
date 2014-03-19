4f96e1c7ad6690291ae046b92f01aad3cedaa045
Mauvis Ledford <mauvis@brainswap.me>
2014-03-19 15:26:05 -0700

    Major update to standardize on grunt/bower stack with soon to have support for Karma test suite.  Add js compress grunt task, add support for jshinting all files grunt task, add grunt connect web server.

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    // Project settings
+    chartstack: {
+      // configurable paths
+      appPath: 'app',
+      scriptPath: 'app/scripts',
+      distPath: 'dist'
+    },
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+        report: 'gzip'
+      },
+      build: {
+        src: [
+            '<%= chartstack.scriptPath %>/chartstack.js',
+            '<%= chartstack.scriptPath %>/chartstack.keen.nvd3.adapter.js',
+            '<%= chartstack.scriptPath %>/chartstack.nvd3.renderer.js'
+        ],
+        dest: '<%= chartstack.distPath %>/chartstack.nvd3.min.js'
+      }
+    },
+
+    jshint: {
+      all: '<%= chartstack.scriptPath %>/*.js'
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          base: '<%= chartstack.appPath %>/',
+          keepalive: true
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+
+};
