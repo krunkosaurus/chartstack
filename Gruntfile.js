d3c7b143687790bfeb85da17cbbe15bc79e4518d
Mauvis Ledford <mauvis@brainswap.me>
2014-04-01 18:26:35 -0700

    Compress latest files to dist.

--- Gruntfile.js
+++ Gruntfile.js
@@ -24,6 +24,7 @@ module.exports = function(grunt) {
         src: [
             '<%= chartstack.scriptPath %>/chartstack.js',
             '<%= chartstack.scriptPath %>/diver/chartstack.diver.js',
+            '<%= chartstack.scriptPath %>/transform/chartstack.csv.js',
             '<%= chartstack.scriptPath %>/adapter/chartstack.keen.universal.adapter.js',
             '<%= chartstack.scriptPath %>/adapter/chartstack.universal.nvd3.adapter.js',
             '<%= chartstack.scriptPath %>/renderer/chartstack.nvd3.renderer.js',
