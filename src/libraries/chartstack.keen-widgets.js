cbca5754408258e318878c1fc3b4d534bd6f3eaf
Dustin Larimer <dustinlarimer@gmail.com>
2014-05-05 14:31:08 -0700

    Update metric class with better style control

--- src/libraries/chartstack.keen-widgets.js
+++ src/libraries/chartstack.keen-widgets.js
@@ -16,37 +16,25 @@
   cs.Keen.Metric = cs.Visualization.extend({
     initialize: function(){
       var css = document.createElement("style");
+      css.id = "cs-styles-keen-io";
       css.type = "text/css";
-      css.innerHTML = ".cs-widget { \
-          background: #49c5b1; \
-          border-radius: 4px; \
-          color: #fff; \
-          font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; \
-          padding: 10px 0; \
-          text-align: center; \
-        } \
-        .cs-widget-title { \
-          display: block; \
-          font-size: 84px; \
-          font-weight: 700; \
-          line-height: 84px; \
-        } \
-        .cs-widget-subtitle { \
-          display: block; \
-          font-size: 24px; \
-          font-weight: 200; \
-        }";
+      css.innerHTML = "\
+.cs-widget { \n  background: #49c5b1; \n  border-radius: 4px; \n  color: #fff; \n  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; \n  padding: 10px 0; \n  text-align: center; \n} \
+.cs-widget-value { \n  display: block; \n  font-size: 84px; \n  font-weight: 700; \n  line-height: 84px; \n} \
+.cs-widget-title { \n  display: block; \n  font-size: 24px; \n  font-weight: 200; \n}";
         // margin: 0 auto; \
-      document.body.appendChild(css);
+      if (!document.getElementById("cs-styles-keen-io")) {
+        document.body.appendChild(css);
+      }
 
       this.render();
     },
     //render: function(){},
     update: function(){
       this.el.innerHTML = '' +
-        '<div class="cs-widget cs-number" style="width:' + parseInt(this.width) + 'px;">' +
-          '<span class="cs-widget-title">' + this.data[0].table[1] + '</span>' +
-          '<span class="cs-widget-subtitle">' + (this.title || 'Result') + '</span>' +
+        '<div class="cs-widget keen-metric" style="width:' + parseInt(this.width) + 'px;">' +
+          '<span class="cs-widget-value">' + this.data[0].table[1] + '</span>' +
+          '<span class="cs-widget-title">' + (this.title || 'Result') + '</span>' +
         '</div>';
     }
   });
