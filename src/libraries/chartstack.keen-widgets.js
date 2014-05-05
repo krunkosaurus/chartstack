df9f55b648064371c333c3bb69ca638803a7149e
Mauvis Ledford <mauvis@brainswap.me>
2014-05-05 14:30:10 -0700

    Refactor chartsack.keen-widgets.js to use .registerLibrary interface.

--- src/libraries/chartstack.keen-widgets.js
+++ src/libraries/chartstack.keen-widgets.js
@@ -1,63 +1,48 @@
 /* global google, chartstack */
 (function(cs){
-  var each = cs.each;
-  var extend = cs.extend;
+  cs.registerLibrary({
+    name: 'Keen',
+    namespace: 'keen-io',
+    charts: [{
+      type : 'Metric',
+      events: {
+        initialize: function(){
+          var css = document.createElement("style");
+          css.type = "text/css";
+          css.innerHTML = ".cs-widget { \
+            background: #49c5b1; \
+            border-radius: 4px; \
+            color: #fff; \
+            font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; \
+            padding: 10px 0; \
+            text-align: center; \
+          } \
+          .cs-widget-title { \
+            display: block; \
+            font-size: 84px; \
+            font-weight: 700; \
+            line-height: 84px; \
+          } \
+          .cs-widget-subtitle { \
+            display: block; \
+            font-size: 24px; \
+            font-weight: 200; \
+          }";
+          // margin: 0 auto; \
+          document.body.appendChild(css);
 
-  // -----------------------------
-  // Library Namespace
-  // -----------------------------
+          this.render();
+        },
 
-  cs.Keen = cs.Keen || {};
-
-  // -----------------------------
-  // Type: Number
-  // -----------------------------
-
-  cs.Keen.Metric = cs.Visualization.extend({
-    initialize: function(){
-      var css = document.createElement("style");
-      css.type = "text/css";
-      css.innerHTML = ".cs-widget { \
-          background: #49c5b1; \
-          border-radius: 4px; \
-          color: #fff; \
-          font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; \
-          padding: 10px 0; \
-          text-align: center; \
-        } \
-        .cs-widget-title { \
-          display: block; \
-          font-size: 84px; \
-          font-weight: 700; \
-          line-height: 84px; \
-        } \
-        .cs-widget-subtitle { \
-          display: block; \
-          font-size: 24px; \
-          font-weight: 200; \
-        }";
-        // margin: 0 auto; \
-      document.body.appendChild(css);
-
-      this.render();
-    },
-    //render: function(){},
-    update: function(){
-      this.el.innerHTML = '' +
-        '<div class="cs-widget cs-number" style="width:' + parseInt(this.width) + 'px;">' +
-          '<span class="cs-widget-title">' + this.data[0].table[1] + '</span>' +
-          '<span class="cs-widget-subtitle">' + (this.title || 'Result') + '</span>' +
-        '</div>';
-    }
-  });
-
-
-  // -----------------------------
-  // Register Methods
-  // -----------------------------
-
-  cs.Visualization.register("keen-io", {
-    "metric": cs.Keen.Metric
+        update: function(){
+          this.el.innerHTML = '' +
+            '<div class="cs-widget cs-number" style="width:' + parseInt(this.width) + 'px;">' +
+            '<span class="cs-widget-title">' + this.data[0].table[1] + '</span>' +
+            '<span class="cs-widget-subtitle">' + (this.title || 'Result') + '</span>' +
+            '</div>';
+        }
+      }
+    }]
   });
 
 })(chartstack);
